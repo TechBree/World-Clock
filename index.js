5c343f518d846a50042d079192044f93745e6d56
TechBree <bridget_kodohu@yahoo.com>
2024-12-22 12:05:27 +0100

    activate js

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-let londonElement = document.querySelector("#london");
-let londonDateElement = londonElement.querySelector(".date");
-let londonTimeElement = londonElement.querySelector(".timing");
-londonDateElement.innerHTML = moment().format("dddd, Do MMMM YYYY");
-londonTimeElement.innerHTML = "1:30:45 <small>PM</small";
+function updateTime() {
+  let londonElement = document.querySelector("#london");
+  let londonDateElement = londonElement.querySelector(".date");
+  let londonTimeElement = londonElement.querySelector(".timing");
+  let londoncode = moment().tz("Europe/London");
+  londonDateElement.innerHTML = londoncode.format("dddd, Do MMMM YYYY");
+  londonTimeElement.innerHTML = londoncode.format(
+    "h:mm:ss [<small>]A[</small>]"
+  );
+
+  let parisElement = document.querySelector("#paris");
+  let parisDateElement = parisElement.querySelector(".date");
+  let parisTimeElement = parisElement.querySelector(".timing");
+  let pariscode = moment().tz("Europe/Paris");
+  parisDateElement.innerHTML = pariscode.format("dddd, Do MMMM YYYY");
+  parisTimeElement.innerHTML = pariscode.format("H:MM:SS [<small>]A[</small>]");
+}
+
+updateTime();
+setInterval(updateLondonTime, 1000);
